c76eef8400e91bb19f5657915f4bce0b62827801
jlstr <jlecampana@gmail.com>
2012-08-03 17:53:16 -0500

    Picture uploading and other Twitter goodness

--- config/application.rb
+++ config/application.rb
@@ -75,7 +75,8 @@ module Ecommerceapp
       g.template_engine :haml
     end
 
-    config.assets.precompile += ['stylesheet.css', 'admin.css', 'bootstrap_and_overrides.css', 'products.css'] 
+    config.assets.precompile += ['stylesheet.css', 'admin.css', 'bootstrap_and_overrides.css', 'products.css',
+                                'main.css' ] 
 
   end
 end
