17e830e69bf5db535259621199525c4f1dc89b26
jlstr <jlecampana@gmail.com>
2012-08-04 09:56:28 -0500

    This was ugly looking..

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
- def picture_for(product)
-  picture = (product.picture_url.nil?) ?
-    image_tag("no-image.jpg", alt: "product") :
-    image_tag(product.picture_url(:thumb).to_s, alt: "product", size: "170x170")  
+  def picture_for(product)
+    picture = (product.picture_url.nil?) ?
+      image_tag("no-image.jpg", alt: "product") :
+      image_tag(product.picture_url(:thumb).to_s, alt: "product", size: "170x170")  
   end
   
 end
