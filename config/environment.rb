95abe7c989c745d53d2c4f95baf213b86e56dae8
jlstr <jlecampana@gmail.com>
2012-08-14 16:07:25 -0500

    Fixing Heroku & Gmail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ecommerceapp::Application.initialize!
+
+Ecommerceapp::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: "587",
+    domain: "gmail.com",
+    authentication: "login",
+    user_name: ENV['GMAIL_SMTP_USERNAME'],
+    password: ENV['GMAIL_SMTP_PASSWORD'],
+    enable_starttls_auto: true
+  }
+  config.action_mailer.raise_delivery_errors = true
+end
